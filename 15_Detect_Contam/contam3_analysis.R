c788477eecb33343d8f9ea3c9d12de042fb4c602
Jennifer Gardner <jgardn92@uw.edu>
2020-09-09 11:38:50 -0700

    contamination work

---
+++ 15_Detect_Contam/contam3_analysis.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+contam <- read.delim("15_Detect_Contam/contamination_stat3.txt")
+
+per.con <- contam$Percentage.of.contaminated.pair...
+hist(per.con, xlim = c(0,30), breaks = 30)
+length(per.con)
+per.con <- sort(per.con)
+sum(per.con>=15)/length(per.con)
+0.95*length(per.con)
+per.con[4235]
