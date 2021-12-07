80d44fd97c1974f933549ccfb7a22bd85b72f6db
Jennifer Gardner <jgardn92@uw.edu>
2021-12-06 22:16:16 -0800

    Running bowtie on 15 more samples up to vcf recode

--- 12_bowtie/bcftools_call.sh
+++ 12_bowtie/bcftools_call.sh
@@ -6,13 +6,13 @@ while read -r GENOME SAMPLEFILE
  do
   echo $GENOME
   echo $SAMPLEFILE
-  touch test2/5_variants/"${GENOME}_call.bcf"
+  touch test3/5_variants/"${GENOME}_call.bcf"
 
   bcftools call --variants-only \
   --consensus-caller \
   --keep-alts \
   --format-fields GQ \
   --output-type b \
-  --output test2/5_variants/"${GENOME}_call.bcf" \
-  test2/4_bam/"${GENOME}_pileup.bcf"
-done < specimen_list.txt
+  --output test3/5_variants/"${GENOME}_call.bcf" \
+  test3/4_bam/"${GENOME}_pileup.bcf"
+done < test3/specimen_list.txt
--- 12_bowtie/vcftools_recode.sh
+++ 12_bowtie/vcftools_recode.sh
@@ -6,10 +6,10 @@ while read -r GENOME SAMPLEFILE
  do
   echo $GENOME
   echo $SAMPLEFILE
-  vcftools --bcf test2/5_variants/"${GENOME}_call.bcf" \
+  vcftools --bcf test3/5_variants/"${GENOME}_call.bcf" \
   --minQ 30 \
   --max-missing 0.75 \
   --recode \
   --recode-INFO-all \
-  --out test2/5_variants/"${GENOME}_call.q30.m20"
-done < specimen_list.txt
+  --out test3/5_variants/"${GENOME}_call.q30.m25"
+done < test3/specimen_list.txt
