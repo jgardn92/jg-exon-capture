6edd7e0eb02cc1d4162ef2da252d003a81f566eb
Jennifer Gardner <jgardn92@uw.edu>
2022-02-03 16:40:44 -0800

    Calculated average read depth for raw and trimmed data

--- 6_Detect_Contamination/15_Detect_Contam/contam3_analysis.R
+++ 6_Detect_Contamination/15_Detect_Contam/contam3_analysis.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-contam <- read.delim("15_Detect_Contam/contamination_stat3.txt")
+
+contam <- read.delim("../jg-exon-capture/6_Detect_Contamination/15_Detect_Contam/contamination_stat3_full.txt")
 
 per.con <- contam$Percentage.of.contaminated.pair...
 hist(per.con, xlim = c(0,30), breaks = 30)
